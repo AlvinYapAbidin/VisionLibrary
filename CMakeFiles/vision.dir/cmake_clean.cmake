3c9743dcc426f3107278a48458a1b59ad0f48553
AlvinYapAbidin <alvin.z.y.a@gmail.com>
2024-02-14 20:56:06 +1100

    cmake

---
+++ CMakeFiles/vision.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vision.dir/main.cpp.o"
+  "CMakeFiles/vision.dir/main.cpp.o.d"
+  "CMakeFiles/vision.dir/vision.cpp.o"
+  "CMakeFiles/vision.dir/vision.cpp.o.d"
+  "vision"
+  "vision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
